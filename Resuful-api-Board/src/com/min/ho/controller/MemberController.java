76f9f0677cd3c1c56576300a6bb160962d202954
MinHoKing <eogh624@gmail.com>
2020-08-26 22:52:36 +0900

    Spring Security 구축 적용 - 1 pom.xml - web.xml - spring-security 까지 작성 개념 이해 요망

--- Resuful-api-Board/src/com/min/ho/controller/MemberController.java
+++ Resuful-api-Board/src/com/min/ho/controller/MemberController.java
@@ -5,4 +5,14 @@ import org.springframework.stereotype.Controller;
 @Controller("/main")
 public class MemberController {
 
+	
+	/*
+	 * https://shinsunyoung.tistory.com/78
+	 * https://sjh836.tistory.com/165
+	 * 스프링 시큐리티 참조 뼈대 개발 착수
+	 * 
+	 * https://thiago6.tistory.com/69
+	 * Spring security 관련 에러 설명
+	 */
+	
 }
